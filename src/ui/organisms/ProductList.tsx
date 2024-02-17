ce3477c398e10259a66a391345be4ed66ec64641
Przemysław Szliep <przemyslaw.szliep@cepd.tech>
2024-02-17 13:15:15 +0100

    added new pages

--- src/ui/organisms/ProductList.tsx
+++ src/ui/organisms/ProductList.tsx
@@ -7,7 +7,10 @@ export const ProductList = ({
 	products: ProductItemType[];
 }) => {
 	return (
-		<ul className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+		<ul
+			className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+			data-testid="products-list
+		>
 			{products.map((product) => {
 				return <ProductListItem key={product.id} product={product} />;
 			})}
