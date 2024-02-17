26a98e7e03de43916b65d2a4468cf587345fb69d
Przemysław Szliep <przemyslaw.szliep@cepd.tech>
2024-02-17 13:17:57 +0100

    fixed data-testid

--- src/ui/organisms/ProductList.tsx
+++ src/ui/organisms/ProductList.tsx
@@ -8,8 +8,8 @@ export const ProductList = ({
 }) => {
 	return (
 		<ul
+			data-testid="products-list"
 			className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
-			data-testid="products-list
 		>
 			{products.map((product) => {
 				return <ProductListItem key={product.id} product={product} />;
