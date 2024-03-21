4e397c071a045a346a2726b671654b3a3d12e723
Przemysław Szliep <przemyslaw.szliep@cepd.tech>
2024-03-21 21:08:02 +0100

    fix

--- src/ui/organisms/ProductList.tsx
+++ src/ui/organisms/ProductList.tsx
@@ -1,19 +1,19 @@
 import { type ProductsListItemFragment } from "@/gql/graphql";
 import { ProductListItem } from "@/ui/molecules/ProductListItem";
 
+type ProductListProps = {
+	products: ProductsListItemFragment[];
+};
+
 export const ProductList = ({
 	products,
-}: {
-	products: ProductsListItemFragment[];
-}) => {
-	return (
-		<ul
-			data-testid="products-list"
-			className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
-		>
-			{products.map((product) => {
-				return <ProductListItem key={product.id} product={product} />;
-			})}
-		</ul>
-	);
-};
+}: ProductListProps): JSX.Element => (
+	<ul
+		className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+		data-testid="products-list"
+	>
+		{products.map((product) => (
+			<ProductListItem key={product.id} product={product} />
+		))}
+	</ul>
+);
